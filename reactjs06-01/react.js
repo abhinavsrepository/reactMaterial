11e1fa50c286657f023387be05bbe169a6bb812e
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-06 23:48:26 +0530

    app initiated

--- reactjs06-01/react.js
+++ reactjs06-01/react.js
@@ -1,5 +1,13 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
 
-function MyComponent() {
-  const intervalRef = useRef(0);
-  const inputRef = useRef(null);
+// function MyComponent() {
+//   const intervalRef = useRef(0);
+//   const inputRef = useRef(null);
+// useref
+
+// import { useState } from 'react';
+
+// function MyComponent() {
+//   const [age, setAge] = useState(28);
+//   const [name, setName] = useState('Taylor');
+//   const [todos, setTodos] = useState(() => createTodos());
